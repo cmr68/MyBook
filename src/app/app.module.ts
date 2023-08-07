554709e800db0a9898cc03fbd14680c45f45d641
Cristina M <cristinamunoz@MacBook-Air-de-Cristina.local>
2023-08-07 21:40:09 +0200

    falta el 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FormRegisterComponent } from './component/form-register/form-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    FormRegisterComponent
   ],
   imports: [
     BrowserModule,
