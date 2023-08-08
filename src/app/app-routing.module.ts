69985f39a97adbc972bd0c65114d928f664b26a2
Cristina M <cristinamunoz@MacBook-Air-de-Cristina.local>
2023-08-08 12:09:09 +0200

    dia 1 OK

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormRegisterComponent } from './component/form-register/form-register.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "home", component: HomeComponent},
+  {path: "form-register", component: FormRegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
