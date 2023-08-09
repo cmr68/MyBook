4751aa62a4d4e05301d51d94798ac6d2a10d3dac
Cristina M <cristinamunoz@MacBook-Air-de-Cristina.local>
2023-08-09 19:24:21 +0200

    falta formulario

--- src/app/pages/porfile/porfile.component.ts
+++ src/app/pages/porfile/porfile.component.ts
@@ -10,7 +10,7 @@ export class PorfileComponent {
   public myUser: User;
 
   constructor(){
-    this.myUser = new User("Francisca", "Perez", "francisaPE@gmail.com", "https://s.t13.cl/sites/default/files/styles/manualcrop_850x475/public/t13/field-imagen/2022-01/1641311995-una-mujer-de-23-aos-y-su-beb-mueren-pisoteados-por-un-elefante.jpg.jpeg?itok=D0CS9-vw","123456")
+    this.myUser = new User("Francisca", "Perez", "francisaPE@gmail.com", "https://s.t13.cl/sites/default/files/styles/manualcrop_850x475/public/t13/field-imagen/2022-01/1641311995-una-mujer-de-23-aos-y-su-beb-mueren-pisoteados-por-un-elefante.jpg.jpeg?itok=D0CS9-vw","123456");
     
   }
 
